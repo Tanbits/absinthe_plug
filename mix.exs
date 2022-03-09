14608de8ee3e782d54c9f394d0a85beb39c0da22
Wasi <48654021+wasitanbits@users.noreply.github.com>
2022-03-09 18:54:23 +0500

    chore: downgrade

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Absinthe.Plug.Mixfile do
 
   defp deps do
     [
-      {:absinthe, "~> 1.6"},
+      {:absinthe, "~> 1.5"},
       {:plug, "~> 1.13"},
       {:jason, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.28.2", only: :dev}
